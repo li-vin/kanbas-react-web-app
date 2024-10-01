9e3eb7556b2c93d50308bb79edbed457640c9d84
li-vin <97973648+li-vin@users.noreply.github.com>
2024-09-30 22:19:58 -0400

    A1 (#1)
    * all of hw1 part 2
    
    * a1 finish 3.2
    
    * add a bunch of courses to dashboard
    
    * p much done
    
    * hopefully fixes babel thing
    
    * add alt text to images
    
    * final deliverables
    
    * final final deliverables

---
+++ src/Labs/TOC.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+export default function TOC() {
+    return (
+        <ul>
+            <li>
+                <Link to="/Labs">Labs</Link>
+            </li>
+            <li>
+                <Link to="/Labs/Lab1">Lab 1</Link>
+            </li>
+            <li>
+                <Link to="/Labs/Lab2">Lab 2</Link>
+            </li>
+            <li>
+                <Link to="/Labs/Lab3">Lab 3</Link>
+            </li>
+            <li>
+                <Link to="/Kanbas/">Kanbas</Link>
+            </li>
+            <li>
+                <Link
+                    id="wd-github"
+                    to="https://github.com/li-vin/kanbas-react-web-app"
+                >
+                    GitHub
+                </Link>
+            </li>
+        </ul>
+    );
+}
